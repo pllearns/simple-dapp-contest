0cc5321d6462c7d82fe9a4c6e628dafe1909de8b
pllearns <pllearns@gmail.com>
2018-01-22 10:55:16 -0800

    bug fix - provider

--- test/Contest.test.js
+++ test/Contest.test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -14,6 +15,7 @@ beforeEach(async () => {
   contest = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000'});
+    contest.setProvider(provider);
 });
 
 describe('Contest contract', () => {
