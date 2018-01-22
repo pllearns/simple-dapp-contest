82349f9befee5115b2357238c0e9a5f488271871
pllearns <pllearns@gmail.com>
2018-01-22 10:11:09 -0800

    Update contest contract

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf-8');
+const contestPath = path.resolve(__dirname, 'contracts', 'Contest.sol');
+const source = fs.readFileSync(contestPath, 'utf-8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Contest'];
 
