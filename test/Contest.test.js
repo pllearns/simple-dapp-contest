164c31289e8242012e8fecfba0e08f815f8a67c9
pllearns <pllearns@gmail.com>
2018-01-22 11:05:33 -0800

    Add test for minimum contest entry

--- test/Contest.test.js
+++ test/Contest.test.js
@@ -60,4 +60,16 @@ describe('Contest contract', () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length);
   });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await contest.methods.enter().send({
+        from: accounts[0],
+        value: 300
+      });
+      assert(false)
+    } catch (err) {
+      assert(err);
+    }
+  });
 });
